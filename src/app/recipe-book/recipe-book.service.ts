8748bf7bc0d87d476e5956e6d998391b38426a70
LuizPPA <luizphilippep@gmail.com>
2018-02-20 16:05:30 -0300

    Adicionadas requisições HTTP

--- src/app/recipe-book/recipe-book.service.ts
+++ src/app/recipe-book/recipe-book.service.ts
@@ -1,40 +1,63 @@
-import { Http } from '@angular/http'
-import { Response } from '@angular/http'
+import { Injectable, OnInit } from '@angular/core'
+import { Http, Response } from '@angular/http'
 import { Subject } from 'rxjs/Subject'
 import { Observable } from 'rxjs/Observable'
 import { Recipe } from './recipe.model'
 import { Ingredient } from '../shared/ingredient.model'
 
-export class RecipeBookService {
+@Injectable()
+export class RecipeBookService implements OnInit{
   recipesChanged: Subject<Recipe[]> = new Subject<Recipe[]>()
   detail: Recipe
-  recipes: Recipe[] = [
-    {
-      name: 'Toast',
-      description: 'Burnt bread',
-      image: 'http://www.khanakhajana.co.in/137-large_default/toast.jpg',
-      ingredients: [new Ingredient('Bread', 2), new Ingredient('Butter', 1)]
-    },
-    new Recipe(
-      'Cake',
-      'A chocolate cake',
-      'https://dochub.com/api/users/20624171.jpg',
-      [new Ingredient('Eggs', 4), new Ingredient('Milk', 2), new Ingredient('Chocolate', 4), new Ingredient('Flour', 1)]
-    )
-  ]
+  recipes: Recipe[]
+  // recipes: Recipe[] = [
+  //   {
+  //     name: 'Toast',
+  //     description: 'Burnt bread',
+  //     image: 'http://www.khanakhajana.co.in/137-large_default/toast.jpg',
+  //     ingredients: [new Ingredient('Bread', 2), new Ingredient('Butter', 1)]
+  //   },
+  //   new Recipe(
+  //     'Cake',
+  //     'A chocolate cake',
+  //     'https://dochub.com/api/users/20624171.jpg',
+  //     [new Ingredient('Eggs', 4), new Ingredient('Milk', 2), new Ingredient('Chocolate', 4), new Ingredient('Flour', 1)]
+  //   )
+  // ]
 
-  constructor(private http: Http){}
+  constructor(private http: Http){
+    this.updateList()
+  }
+
+  ngOnInit(){
+  }
+
+  addOnDatabase(recipe: Recipe){
+    this.http.post('https://udemy-angular-88cef.firebaseio.com/recipes.json', recipe).subscribe((response: Response) => {
+      console.log(response)
+    })
+  }
+
+  updateDatabase(){
+    this.http.put('https://udemy-angular-88cef.firebaseio.com/recipes.json', this.recipes).subscribe((response: Response) => {
+      console.log(response)
+    })
+  }
+
+  updateList(){
+    this.http.get('https://udemy-angular-88cef.firebaseio.com/recipes.json').subscribe((response: Response) => {
+      this.recipes = response.json()
+      this.recipesChanged.next(this.getRecipes())
+    })
+  }
 
   add(recipe: Recipe){
     this.recipes.push(recipe)
-    this.http.put('https://udemy-angular-88cef.firebaseio.com/recipes', recipe)
+    this.updateDatabase()
     this.recipesChanged.next(this.getRecipes())
   }
 
   getRecipes(){
-    // this.http.get('https://udemy-angular-88cef.firebaseio.com/recipes').subscribe((response: Response) => {
-    //   this.recipes = response.json()
-    // })
     return this.recipes
   }
 
@@ -48,6 +71,7 @@ export class RecipeBookService {
 
   update(i: number, recipe: Recipe){
     this.recipes[i] = recipe
+    this.updateDatabase()
     this.recipesChanged.next(this.getRecipes())
   }
 
